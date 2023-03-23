420ca5f9ebe77eaffa2fd6d828d5a204e5a83465
aleksakovacevic01 <128324791+aleksakovacevic01@users.noreply.github.com>
2023-03-23 21:37:40 +0100

    My second commit

---
+++ Hallo_World.py
@@ -0,0 +1,28 @@
+##print("Hello Python woled!")
+##poruka = "Cao"
+##print(poruka)
+##name = "Bilo sta"
+##print(name.title())
+##name = "nenene"
+##print(name.upper())
+##print(name.lower())
+##first_name = "Petar"
+##last_name = "Kovacvic"
+##print(recenica)
+
+# braca = ["luka","Boki","Petar"]
+# print(f"Moj rodjeni brat se zove  braca[0]}")
+# favorite_language ='python '
+# print(favorite_language)
+# favorite_language.rstrip()
+# print(favorite_language)
+# favorite_language = favorite_language.rstrip()
+# print(favorite_language)
+# message = "One of python's strengths is its diverse community "
+# print(message)
+# message = 'One of python's strengths is its diverse community
+# print(message)
+# print(2*0.3)
+# x, y, z, = 0, 0, 3
+# print(x+z)
+# print("/tPahton")
---
+++ WORKING_WITH_LISTS.py
@@ -0,0 +1,66 @@
+#  magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     # print(magician)
+#     print(f"{magician.title()}, that was a great trick")
+#     print(f"I can't wait to see you next trick, {magician.title()}.\n")
+# print("Thank you eveyone. That was a great magic show")
+# for value in range(1, 5):
+#     print(value)
+# for value in range(1, 6):
+#     print(value)
+# numbers = list(range(1,6))
+# print(numbers)
+# even_numbers = list(range(2,11, 2))
+# print(even_numbers)
+# squares = []
+# for value in range(1, 11):
+#     square = value ** 2
+#     squares.append(square)
+# print(squares)
+# squares = []
+# for value in range(1,11):
+#     squares.append(value**2)
+# print(squares)
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(min(digits))
+# max(digits)
+# sum(digits)
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[0:3])
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[:4])
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[2:])
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print("Here are the first trhee players on my team")
+# for player in players[:3]:
+#     print(player.title())
+# my_food = ['pizza', 'falafel', 'carrot cake']
+# my_friend_foods = my_food[:]
+# print("My favorite foods are:")
+# print(my_food)
+# print("\nMy friends favorite food are:")
+# print(my_friend_foods)
+# my_food.append('cannoli')
+# my_friend_foods.append('ice cream')
+# print("My favorite foods are:")
+# print(my_food)
+# print("\nMy friends favorite food are:")
+# print(my_friend_foods)
+# dimensions = (200, 50)
+# print(dimensions[0])
+# print(dimensions[1])
+
+
+
+
+
+
+
+
+
+
+
+
