ec7f3db69248b87f4e0693449c399b85c4b804ae
aleksakovacevic01 <128324791+aleksakovacevic01@users.noreply.github.com>
2023-03-23 21:30:49 +0100

    Uncomment code

--- INTRODUCING_LISTS.py
+++ INTRODUCING_LISTS.py
@@ -1,40 +1,40 @@
-# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-# print(bicycles)
-# print(bicycles[0])
-# print(bicycles[0].title())
-# print(bicycles[1],"23323",bicycles[3])
-# message = f"My first bicycles was a {bicycles[0].title()}"
-# print(message)
-# motorccycles = ['honda', 'yamaha', 'suzuki']
-# print(motorccycles)
-# motorccycles[0] = 'ducati'
-# print(motorccycles)
-# motorccycles.append('ducati')
-# print(motorccycles)
-# motorcycles = []
-# motorcycles.append('honda')
-# motorcycles.append('yamaha')
-# motorcycles.append('suzuki')
-# print(motorcycles)
-# motorcycles = ['honda', 'yamaha', 'suzuki']
-# motorcycles.insert(3, 'ducati')
-# print(motorcycles)
-# del motorcycles[0]
-# print(motorcycles)#
-# motorcycles = ['honda', 'yamaha', 'suzuki']
-# print(motorcycles)
-# popped_motorcycle = motorcycles.pop()
-# print(motorcycles)
-# first_owned = motorcycles.pop(0)
-# print(f"The first motorcycle I owned was a {first_owned.title()}.")
-# motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
-# print(motorcycles)
-# motorcycles.remove('ducati')
-# print(motorcycles)
-# cars = ['bmw', 'audi', 'toyota', 'subaru']
-# cars.sort()
-# print(cars)
-# print("Here is the original list:")
-# print(cars)
-# print("\nHere is the sorted list:")
-# print(sorted(cars))
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1],"23323",bicycles[3])
+message = f"My first bicycles was a {bicycles[0].title()}"
+print(message)
+motorccycles = ['honda', 'yamaha', 'suzuki']
+print(motorccycles)
+motorccycles[0] = 'ducati'
+print(motorccycles)
+motorccycles.append('ducati')
+print(motorccycles)
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(3, 'ducati')
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)#
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+print("Here is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
