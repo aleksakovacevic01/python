a2f34e46f48283c352a6a8f96e70c07930283e5a
aleksakovacevic01 <128324791+aleksakovacevic01@users.noreply.github.com>
2023-03-25 21:34:45 +0100

    new coomit

---
+++ IF_STATEMENTS.py
@@ -0,0 +1,64 @@
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+# requested_topping = 'mushrooms'
+# if requested_topping != 'anchovies':
+#     print("Hold the anchovies!")
+# else:
+#     print("err")
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'marie'
+# if user not in banned_users:
+#     print(f"{user.title()}, you can post a response if you wish.")
+# age = 18
+# if age >= 18:
+#     print("you are old enough to vote!")
+# else:
+#     print("you aren't old enough to vote")
+# age = 17
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
+# age = 12
+# if age < 4:
+#     print("Your admission cost is $0.")
+# elif age < 18:
+#     print("Your admission cost is $25.")
+# else:
+#     print("Your admission cost is $40.")
+# requested_toppings = ['mushrooms', 'extra cheese']
+# if 'mushrooms' in requested_toppings:
+#     print("Adding mushrooms.")
+# elif 'pepperoni' in requested_toppings:
+#     print("Adding pepperoni.")
+# elif 'extra cheese' in requested_toppings:
+#     print("Adding extra cheese.")
+# print("\nFinished making your pizza!")
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# for requested_topping in requested_toppings:
+#     if requested_topping == 'green peppers':
+#         print("Sorry, we are out of green peppers right now.")
+#     else:
+#         print(f"Adding {requested_topping}.")
+# print("\nFinished making your pizza!")
+# reguested_toppings = ['www', 'rrr']
+# if reguested_toppings:
+#     for reguested_topping in reguested_toppings:
+#         print(f"adding {reguested_topping}.")
+# else:
+#     print("Are you sure you want a plain pizza")
+# available_toppings = ['mushrooms', 'olives', 'green peppers',
+# 'pepperoni', 'pineapple', 'extra cheese']
+# requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+# for requested_topping in requested_toppings:
+#     if requested_topping in available_toppings:
+#         print(f"Adding {requested_topping}.")
+#     else:
+#         print(f"Sorry, we don't have {requested_topping}.")
+# print("\nFinished making your pizza!")
